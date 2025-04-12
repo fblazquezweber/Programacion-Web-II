9c5d59ba3119d7f845c857bdddd61a9daccdad26
Francisco Blazquez Weber <156860951+fblazquezweber@users.noreply.github.com>
2025-04-13 00:46:05 +0200

    Delete Semana 8 directory

--- Semana 8/admin/cerrar_sesion_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Asegura que la sesión esté iniciada
-session_destroy(); // Destruye la sesión
-header("Location: ../home.html"); // Redirige a la página de inicio (home.html)
-exit();
-?>
